b15cc719d014a614acf828e46d50b76c3af201a8
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-11-06 20:28:28 +0100

    Job Scheduler init

--- app/src/main/java/com/okwy/practiceproject/JobScheduler/JobSchedulerActivity.java
+++ app/src/main/java/com/okwy/practiceproject/JobScheduler/JobSchedulerActivity.java
@@ -12,5 +12,6 @@ public class JobSchedulerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_scheduler);
+
     }
 }
