d7937c0a520b1fd5203933a9b5ab96a03b213a77
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2020-11-26 00:50:52 +0100

    Added WorkManager +

--- app/src/main/java/com/okwy/practiceproject/WorkManager/MyWorker.java
+++ app/src/main/java/com/okwy/practiceproject/WorkManager/MyWorker.java
@@ -1,12 +1,22 @@
 package com.okwy.practiceproject.WorkManager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.okwy.practiceproject.R;
+
 public class MyWorker extends Worker {
+
+    public static final String KEY_OUTPUT_DESC = "key_output_desc";
 
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -15,6 +25,33 @@ public class MyWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        Data data = getInputData();
+        String desc = data.getString(WorkManagerActivity.KEY_INPUT_DESC);
+
+        displayNotification(desc);
+
+        Data outData = new Data.Builder()
+                .putString(KEY_OUTPUT_DESC, "Task Complete")
+                .build();
+
+        
+        return Result.success();
+    }
+
+
+    private void displayNotification(String desc){
+        NotificationManager notifyManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("channel_id", "channel_name", NotificationManager.IMPORTANCE_HIGH);
+            notifyManager.createNotificationChannel(channel);
+        }
+
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(), "channel_id")
+                .setContentTitle("Workmanager")
+                .setContentText(desc+ "\nThis is the work you made")
+                .setSmallIcon(R.drawable.ic_job_running)
+                .build();
+
+        notifyManager.notify(109, notification);
     }
 }
--- app/src/main/java/com/okwy/practiceproject/WorkManager/WorkManagerActivity.java
+++ app/src/main/java/com/okwy/practiceproject/WorkManager/WorkManagerActivity.java
@@ -1,16 +1,50 @@
 package com.okwy.practiceproject.WorkManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.okwy.practiceproject.R;
 
 public class WorkManagerActivity extends AppCompatActivity {
 
+    private TextView workStatus;
+    private OneTimeWorkRequest request;
+    public static final String KEY_INPUT_DESC = "key_input_desc";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_manager);
+
+        Data data = new Data.Builder()
+                .putString(KEY_INPUT_DESC, "Putting in the data for the work")
+                .build();
+
+        request = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .setInputData(data)
+                .build();
+
+        workStatus = findViewById(R.id.workStatus);
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                String status = workInfo.getState().name();
+                workStatus.append(status + "\n");
+            }
+        });
+
+    }
+
+    public void performWork(View view) {
+        WorkManager.getInstance(this).enqueue(request);
     }
 }
